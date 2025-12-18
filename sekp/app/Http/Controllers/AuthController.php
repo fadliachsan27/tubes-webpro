474e09d98ae4c41d20f8d7939073b5ac5e747e00
khalishs <91864109+khalishs@users.noreply.github.com>
2025-12-18 15:02:36 +0700

    sec commit

--- sekp/app/Http/Controllers/AuthController.php
+++ sekp/app/Http/Controllers/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller
                 'admin' => redirect('/admin'),
                 'manager' => redirect('/manager'),
                 'karyawan' => redirect('/karyawan'),
-                default => redirect('/user'),
+                default => redirect('/admin'),
             };
         }
 
